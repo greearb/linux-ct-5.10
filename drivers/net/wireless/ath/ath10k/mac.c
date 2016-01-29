bc380558d190ae8121cfe5d54b67c813888c41d4
Ben Greear <greearb@candelatech.com>
2016-01-28 18:04:22 -0800

    ath10k: Support more than one station when using CT firmware.
    CT firmware can support more than one station, and will also be
    able to handle the other module-param tunings, so go ahead and
    add support for that to the driver.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/mac.c
+++ drivers/net/wireless/ath/ath10k/mac.c
@@ -10119,6 +10119,7 @@ static struct ieee80211_iface_combination ath10k_10x_ct_if_comb[] = {
 		.max_interfaces = DEF_TARGET_10X_NUM_VDEVS_CT,
 		.num_different_channels = 1,
 		.beacon_int_infra_match = true,
+		.beacon_int_min_gcd = 1,
 #ifdef CONFIG_ATH10K_DFS_CERTIFIED
 		.radar_detect_widths =	BIT(NL80211_CHAN_WIDTH_20_NOHT) |
 					BIT(NL80211_CHAN_WIDTH_20) |
@@ -10216,6 +10217,36 @@ ieee80211_iface_combination ath10k_10_4_bcn_int_if_comb[] = {
 					BIT(NL80211_CHAN_WIDTH_80) |
 					BIT(NL80211_CHAN_WIDTH_80P80) |
 					BIT(NL80211_CHAN_WIDTH_160),
+#endif
+	},
+};
+
+static struct ieee80211_iface_limit ath10k_10_4_ct_if_limits[] = {
+	{
+		.max = 1,
+		.types = BIT(NL80211_IFTYPE_STATION),
+	},
+	{
+		.max	= 16,
+		.types	= BIT(NL80211_IFTYPE_AP)
+#ifdef CONFIG_MAC80211_MESH
+			| BIT(NL80211_IFTYPE_MESH_POINT)
+#endif
+	},
+};
+
+static struct ieee80211_iface_combination ath10k_10_4_ct_if_comb[] = {
+	{
+		.limits = ath10k_10_4_ct_if_limits,
+		.n_limits = ARRAY_SIZE(ath10k_10_4_ct_if_limits),
+		.max_interfaces = 16,
+		.num_different_channels = 1,
+		.beacon_int_infra_match = true,
+#ifdef CONFIG_ATH10K_DFS_CERTIFIED
+		.radar_detect_widths =	BIT(NL80211_CHAN_WIDTH_20_NOHT) |
+					BIT(NL80211_CHAN_WIDTH_20) |
+					BIT(NL80211_CHAN_WIDTH_40) |
+					BIT(NL80211_CHAN_WIDTH_80),
 #endif
 	},
 };
@@ -10682,15 +10713,45 @@ int ath10k_mac_register(struct ath10k *ar)
 		}
 		break;
 	case ATH10K_FW_WMI_OP_VERSION_10_4:
-		ar->hw->wiphy->iface_combinations = ath10k_10_4_if_comb;
-		ar->hw->wiphy->n_iface_combinations =
-			ARRAY_SIZE(ath10k_10_4_if_comb);
-		if (test_bit(WMI_SERVICE_VDEV_DIFFERENT_BEACON_INTERVAL_SUPPORT,
-			     ar->wmi.svc_map)) {
+		if (test_bit(ATH10K_FW_FEATURE_WMI_10X_CT,
+			     ar->running_fw->fw_file.fw_features)) {
+			ATH_ASSIGN_CONST_U16(ath10k_10_4_ct_if_comb[0].limits[0].max, ar->max_num_vdevs);
+			ath10k_10_4_ct_if_comb[0].max_interfaces =
+				ar->max_num_vdevs;
+			if (ath10k_10_4_ct_if_comb[0].limits[1].max > ar->max_num_vdevs)
+				ATH_ASSIGN_CONST_U16(ath10k_10_4_ct_if_comb[0].limits[1].max, ar->max_num_vdevs);
+
+			if (test_bit(WMI_SERVICE_VDEV_DIFFERENT_BEACON_INTERVAL_SUPPORT,
+				     ar->wmi.svc_map)) {
+				ath10k_10_4_ct_if_comb[0].beacon_int_min_gcd = 100;
+			}
+			else {
+				ath10k_10_4_ct_if_comb[0].beacon_int_min_gcd = 1;
+			}
+
 			ar->hw->wiphy->iface_combinations =
-				ath10k_10_4_bcn_int_if_comb;
+				ath10k_10_4_ct_if_comb;
 			ar->hw->wiphy->n_iface_combinations =
-				ARRAY_SIZE(ath10k_10_4_bcn_int_if_comb);
+				ARRAY_SIZE(ath10k_10_4_ct_if_comb);
+			ar->hw->wiphy->interface_modes |= BIT(NL80211_IFTYPE_ADHOC);
+
+			/* CT firmware can do tx-sw-crypt if properly configured */
+			if (test_bit(ATH10K_FW_FEATURE_CT_RXSWCRYPT,
+				     ar->running_fw->fw_file.fw_features) &&
+			    ath10k_modparam_nohwcrypt)
+				__clear_bit(IEEE80211_HW_SW_CRYPTO_CONTROL, ar->hw->flags);
+
+		} else {
+			ar->hw->wiphy->iface_combinations = ath10k_10_4_if_comb;
+			ar->hw->wiphy->n_iface_combinations =
+				ARRAY_SIZE(ath10k_10_4_if_comb);
+			if (test_bit(WMI_SERVICE_VDEV_DIFFERENT_BEACON_INTERVAL_SUPPORT,
+			     ar->wmi.svc_map)) {
+				ar->hw->wiphy->iface_combinations =
+					ath10k_10_4_bcn_int_if_comb;
+				ar->hw->wiphy->n_iface_combinations =
+					ARRAY_SIZE(ath10k_10_4_bcn_int_if_comb);
+			}
 		}
 		break;
 	case ATH10K_FW_WMI_OP_VERSION_UNSET:
