01e0d4232221d84fb1d0d6eb5092d10417601055
Ben Greear <greearb@candelatech.com>
2016-02-11 18:46:22 -0800

    ath10k: Fix endian issue when dumping crash logs.
    This should make it work on systems with different CPU endian-ness
    from that of the firmware.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/pci.c
+++ drivers/net/wireless/ath/ath10k/pci.c
@@ -2028,7 +2028,7 @@ static void ath10k_pci_dump_dbglog(struct ath10k *ar)
 
 		ath10k_dbg_save_fw_dbg_buffer(ar, (__le32 *)(buffer), len >> 2);
 		ath10k_dbg_print_fw_dbg_buffer(ar, (__le32 *)(buffer),
-					       dbuf.length/sizeof(__le32),
+					       len / sizeof(__le32),
 					       KERN_ERR);
 		kfree(buffer);
 
