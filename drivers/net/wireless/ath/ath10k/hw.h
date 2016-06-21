f4b3105c72d6b81d0e4646de6aa6442c8458bd6a
Ben Greear <greearb@candelatech.com>
2016-06-21 15:04:45 -0700

    ath10k: Fix OOM issue in 'full' firmware image.
    The default number of vdevs was too high.  Make it only 16, and
    let users configure it higher using fwcfg and/or module-param
    as desired.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/hw.h
+++ drivers/net/wireless/ath/ath10k/hw.h
@@ -735,8 +735,8 @@ ath10k_is_rssi_enable(struct ath10k_hw_params *hw,
 						 (TARGET_10X_NUM_VDEVS))
 
 /* Over-rides for Candela Technologies firmware */
-#define DEF_TARGET_10X_NUM_VDEVS_CT		36 /* newer CT firmware support more,
-						    * override w/module parm */
+#define DEF_TARGET_10X_NUM_VDEVS_CT		16 /* Can support up to 64 with proper config of other settings.
+						    * override w/module parm or fwcfg file */
 #define TARGET_10X_AST_SKID_LIMIT_CT		(ath10k_modparam_target_num_peers_ct * TARGET_10X_NUM_PEER_AST)
 #define TARGET_10X_NUM_PEER_KEYS_CT             (WMI_MAX_KEY_INDEX + 1) /* 4 */
 
