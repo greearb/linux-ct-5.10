297e27062283e86822756024e26cc08607aa25fe
Ben Greear <greearb@candelatech.com>
2016-10-06 17:08:25 -0700

    ath10k: Disable MU-beamformee if using rx-sw-crypt mode.
    For one reason or another, MU-MIMO does not work in that case,
    so disable it from the capabilities so we don't confuse the higher
    stack (although it didn't seem to really hurt anything, we just didn't
    get MU-MIMO type throughput).
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/wmi.c
+++ drivers/net/wireless/ath/ath10k/wmi.c
@@ -5624,6 +5624,13 @@ static void ath10k_wmi_event_service_ready_work(struct work_struct *work)
 	ath10k_dbg(ar, ATH10K_DBG_WMI, "wmi sys_cap_info 0x%x\n",
 		   ar->sys_cap_info);
 
+	// RX Sw Crypt mode, which uses raw receive and disables some HW rx logic,
+	// appears to break receiving MU-MIMO properly, so tweak that here.
+	if (test_bit(ATH10K_FW_FEATURE_CT_RXSWCRYPT,
+		     ar->running_fw->fw_file.fw_features) &&
+	    ar->request_nohwcrypt)
+		ar->vht_cap_info &= ~IEEE80211_VHT_CAP_MU_BEAMFORMEE_CAPABLE;
+
 	if (ar->num_rf_chains > ar->max_spatial_stream) {
 		ath10k_warn(ar, "hardware advertises support for more spatial streams than it should (%d > %d)\n",
 			    ar->num_rf_chains, ar->max_spatial_stream);
