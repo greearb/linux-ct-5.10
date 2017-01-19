366cfa401996011f06ceb8aa8d19870b2b65cc0c
Ben Greear <greearb@candelatech.com>
2017-01-19 13:39:09 -0800

    ath10k: Fix performance on 2x2 NICs running 3x3 firmware.
    This resolves rate-control issues related to CT firmware when
    the firmware is compiled for a different number of NSS than the
    NIC actually supports.  This caused 2x2 NICs running 3x3
    988X firmware to not do VHT rates well, for instance.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/pci.c
+++ drivers/net/wireless/ath/ath10k/pci.c
@@ -3856,7 +3856,8 @@ static int ath10k_pci_probe(struct pci_dev *pdev,
 	int (*pci_hard_reset)(struct ath10k *ar);
 	u32 (*targ_cpu_to_ce_addr)(struct ath10k *ar, u32 addr);
 
-	printk(KERN_INFO "ath10k driver, optimized for CT firmware, probing pci.\n");
+	printk(KERN_INFO "ath10k driver, optimized for CT firmware, probing pci device: 0x%x.\n",
+	       pci_dev->device);
 
 	switch (pci_dev->device) {
 	case QCA988X_2_0_DEVICE_ID_UBNT:
