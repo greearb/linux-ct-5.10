60b39cd98ce0b51fdcfa6a8ef860abf1fb130292
Ben Greear <greearb@candelatech.com>
2018-02-27 17:20:29 -0800

    ath10k: Attempt to work around napi_synchronize hang.
    Calling napi_disable twice in a row (w/out starting it and/or without
    having NAPI active leads to deadlock because napi_disable sets
    NAPI_STATE_SCHED and NAPI_STATE_NPSVC when it returns, as far as I
    can tell.  So, guard this call to napi_disable.  I believe the
    failure case is something like this:
     rmmod ath10k_pci ath10k_core
       Firmware crashes before hif_stop is called by the rmmod path
       The crash handling logic calls hif_stop
       Then rmmod gets around to calling hif_stop, but spins endlessly
       in napi_synchronize.
    
    I think one way this could happen is that ath10k_stop checks
    for state != ATH10K_STATE_OFF, but STATE_RESTARTING is also
    a possibility.  That might be how we can have hif_stop called twice
    without a hif_start in between. --Ben
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/ahb.c
+++ drivers/net/wireless/ath/ath10k/ahb.c
@@ -627,6 +627,8 @@ static int ath10k_ahb_hif_start(struct ath10k *ar)
 	ath10k_dbg(ar, ATH10K_DBG_BOOT, "boot ahb hif start\n");
 
 	napi_enable(&ar->napi);
+	ar->napi_enabled = true;
+
 	ath10k_ce_enable_interrupts(ar);
 	ath10k_pci_enable_legacy_irq(ar);
 
@@ -644,8 +646,11 @@ static void ath10k_ahb_hif_stop(struct ath10k *ar)
 	ath10k_ahb_irq_disable(ar);
 	synchronize_irq(ar_ahb->irq);
 
-	napi_synchronize(&ar->napi);
-	napi_disable(&ar->napi);
+	if (ar->napi_enabled) {
+		napi_synchronize(&ar->napi);
+		napi_disable(&ar->napi);
+		ar->napi_enabled = false;
+	}
 
 	ath10k_pci_flush(ar);
 }
