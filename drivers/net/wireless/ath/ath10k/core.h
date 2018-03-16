88b3e90ddfd97e48e694397dec899d936f3f824b
Ben Greear <greearb@candelatech.com>
2018-03-16 12:46:02 -0700

    ath10k: Support disable-cca-ibss set-special hack.
    Possibly this will improve IBSS in some cases, test results were
    inconclusive.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -1558,6 +1558,7 @@ struct ath10k {
 		bool coverage_already_set;
 		bool txbf_cv_msg;
 		bool rx_all_mgt;
+		u8 disable_ibss_cca;
 		u8 rc_txbf_probe;
 #define CT_DISABLE_20MHZ  0x1
 #define CT_DISABLE_40MHZ  0x2
