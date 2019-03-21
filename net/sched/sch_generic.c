3785a9b74dae330740f3816144a39828772bd61f
Ben Greear <greearb@candelatech.com>
2019-03-21 11:33:41 -0700

    sch-generic: Fix compile (backport)
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- net/sched/sch_generic.c
+++ net/sched/sch_generic.c
@@ -672,6 +672,7 @@ static int pfifo_fast_try_enqueue(struct sk_buff *skb, struct Qdisc* qdisc, stru
 	int band = prio2band[skb->priority & TC_PRIO_MAX];
 	struct pfifo_fast_priv *priv = qdisc_priv(qdisc);
 	struct skb_array *q = band2list(priv, band);
+	unsigned int pkt_len = qdisc_pkt_len(skb);
 	int err;
 
 	err = skb_array_produce(q, skb);
@@ -688,8 +689,7 @@ static int pfifo_fast_try_enqueue(struct sk_buff *skb, struct Qdisc* qdisc, stru
 		}
 	}
 
-	qdisc_qstats_cpu_qlen_inc(qdisc);
-	qdisc_qstats_cpu_backlog_inc(qdisc, skb);
+	qdisc_update_stats_at_enqueue(qdisc, pkt_len);
 	return NET_XMIT_SUCCESS;
 }
 
